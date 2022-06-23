b6d16f091742c61571bcefa1a7e69926c89c9f35
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-06-23 10:00:17 +0100

    made connect button reusable

--- pages/components/Button/Connect/index.js
+++ pages/components/Button/Connect/index.js
@@ -2,23 +2,22 @@ import styled from "styled-components";
 
 const ConnectButton = styled.button`
     width: 100%;
-    height: 40px;
-    background: #101010 !important;
+    height: ${(props) => props.height || "40px"};
+    background-color: ${(props) => props.backgroundColor || "#101010"};
     border: none !important;
     border-radius: 9999px;
     outline: none !important;
     text-align: center;
-    font-size: 14px;
-    color: #fff !important;
+    font-size: ${(props) => props.fontSize || "14px"};
+    color: ${(props) => props.color || "#fff"};
     vertical-align: middle;
     position: relative;
     z-index: 1;
     overflow: hidden;
     animation: all .3s ease;
+    padding:0 10px;
     :hover{
-        color: #878bff;
-        border-color: #878bff;
-        background: #fff;
+        cursor:pointer;
     }
 `
 
