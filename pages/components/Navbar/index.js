03424385d1fb655c0c2cb62fdb5b49375ea71fa1
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-06-23 11:05:07 +0100

    updated nav component

--- pages/components/Navbar/index.js
+++ pages/components/Navbar/index.js
@@ -1,91 +1,115 @@
 import styled from "styled-components";
-import Image from 'next/image';
-import { IoMdNotifications } from 'react-icons/io';
-import Button from '../Button/Connect';
+import Image from "next/image";
+import { IoMdNotifications } from "react-icons/io";
+import { GiHamburgerMenu } from "react-icons/gi";
+import Button from "../Button/Connect";
 
 const NavbarContainer = styled.div`
-    position:sticky;
-    max-width:100%;
-    height:75px;
-    padding:1rem 0.8rem;
-    background-color:${(props) => props.backgroundColor};
-    display:flex;
-    flex-direction:row;
-    justify-content:space-between;
-    align-items:center;
-`
+  position: sticky;
+  max-width: 100%;
+  height: 75px;
+  padding: 1rem 0.8rem;
+  background-color: ${(props) => props.backgroundColor};
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-const NavLogo = styled.div`
-
-`
+const NavLogo = styled.div``;
 
 const NavLinkContainer = styled.div`
-
-    ul{
-        list-style:none;
-        display:flex;
-        justify-content:space-between;
-        column-gap:1rem;
-    }
-    li{
-        padding:5px 15px;
-    }
-    li:hover{
-        cursor:pointer;
-    }
-    .active_link{
-        background-color:#101010;
-        color:#fff;
-        border-radius:30px;
-    }
-`
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    column-gap: 1rem;
+  }
+  li {
+    padding: 5px 15px;
+  }
+  li:hover {
+    cursor: pointer;
+  }
+  .active_link {
+    background-color: #101010;
+    color: #fff;
+    border-radius: 30px;
+  }
+`;
 
 const NotificationContainer = styled.div`
-    background-color: rgba(255,255,255,.5);
-    padding:5px;
-    border-radius:50%;
-    width:25px;
-    height:25px;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-`
+  background-color: rgba(255, 255, 255, 0.5);
+  padding: 5px;
+  border-radius: 50%;
+  width: 25px;
+  height: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const NavColumn = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  @media (max-width: 480px) {
+    display: none;
+  }
+`;
 
 const ConnectButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 1rem;
+`;
 
-`
+const Menu = styled.div`
+  display: none;
+  :hover {
+    cursor: pointer;
+  }
+`;
 
-
-
-const Navbar = () =>{
-    return(
-        <>
-            <NavbarContainer>
-                <NavLogo>
-                    <Image src='/logo.svg' width={106} height={30} />
-                </NavLogo>
-                <NavLinkContainer>
-                    <ul>
-                        <li>Wallet</li>
-                        <li className="active_link">Bridge</li>
-                        <li>Staking</li>
-                        <li>Converter</li>
-                        <li>BTT Redomination</li>
-                    </ul>
-                </NavLinkContainer>
-                <NotificationContainer>
-                    <IoMdNotifications />
-                </NotificationContainer>
-                <ConnectButtonContainer>
-                    <Button
-                        height="36px"
-                        backgroundColor="rgba(255,255,255,.5)"
-                        color="#fff"
-                    />
-                </ConnectButtonContainer>
-            </NavbarContainer>
-        </>
-    )
-}
+const Navbar = () => {
+  return (
+    <>
+      <NavbarContainer>
+        <NavLogo>
+          <Image src="/logo.svg" width={106} height={30} />
+        </NavLogo>
+        <NavColumn>
+          <NavLinkContainer>
+            <ul>
+              <li>Wallet</li>
+              <li className="active_link">Bridge</li>
+              <li>Staking</li>
+              <li>Converter</li>
+              <li>BTT Redomination</li>
+            </ul>
+          </NavLinkContainer>
+          <ConnectButtonContainer>
+            <NotificationContainer>
+              <IoMdNotifications />
+            </NotificationContainer>
+            <Button
+              height="36px !important"
+              backgroundColor="rgba(255,255,255,.5) !important"
+              color="#000"
+              hoverBackgroundColor="rgba(255,255,255,.3) !important"
+            />
+          </ConnectButtonContainer>
+        </NavColumn>
+        <Menu>
+          <GiHamburgerMenu color="#fff" size={25} />
+        </Menu>
+      </NavbarContainer>
+    </>
+  );
+};
 
 export default Navbar;
