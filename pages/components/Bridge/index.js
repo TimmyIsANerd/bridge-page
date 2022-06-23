78020718accb81c2ed86c226ab25b4c2bdafe7e8
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-06-23 11:04:50 +0100

    updated component

--- pages/components/Bridge/index.js
+++ pages/components/Bridge/index.js
@@ -1,23 +1,28 @@
-import styled from 'styled-components';
-import Button from '../Button/Connect';
+import styled from "styled-components";
+import Button from "../Button/Connect";
 
 const Container = styled.div`
-    border-radius: 26px;
-    background: #fff;
-    min-height: 300px;
-    width:700px;
-    margin:1.5rem 0;
-    padding: 1.5rem;
-`
+  border-radius: 26px;
+  background: #fff;
+  min-height: 300px;
+  width: 700px;
+  margin: 1.5rem 0;
+  padding: 1.5rem;
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+  @media (max-width: 480px) {
+    width: 80%;
+  }
+`;
 
 const Heading = styled.h3`
-    font-weight:500;
-
-`
+  font-weight: 500;
+`;
 
 const ButtonContainer = styled.div`
-    width:100%;
-`
+  width: 100%;
+`;
 
 const FormContainer = styled.div``;
 
@@ -27,25 +32,22 @@ const FormGroup = styled.div``;
 
 const TextInput = styled.input``;
 
+const Bridge = () => {
+  return (
+    <>
+      <Container>
+        <Heading>Bridge</Heading>
+        <hr />
 
-const Bridge = () =>{
-    return(
-        <>
-            <Container>
-                <Heading>Bridge</Heading>
-                <hr />
-
-                <FormContainer>
-                    <Form>
-                        
-                    </Form>
-                </FormContainer>
-                <ButtonContainer>
-                    <Button />
-                </ButtonContainer>
-            </Container>
-        </>
-    )
-}
+        <FormContainer>
+          <Form></Form>
+        </FormContainer>
+        <ButtonContainer>
+          <Button />
+        </ButtonContainer>
+      </Container>
+    </>
+  );
+};
 
 export default Bridge;
