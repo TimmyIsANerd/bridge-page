a314e7a96182068453154c3bfc98cef26fef7fa5
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-06-24 08:58:25 +0100

    placed context wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import dynamic from 'next/dynamic';
+
+const GlobalContextProvider = dynamic(() => import('../context/globalContext'), { ssr: false });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <GlobalContextProvider>
+      <Component {...pageProps} />
+    </GlobalContextProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
