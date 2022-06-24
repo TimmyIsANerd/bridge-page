462d54dbe55fc62c52b85355bf38b24e7192da26
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-06-24 09:02:16 +0100

    setting page attributes in docent

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <title>Bridge Page</title>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
