3446c86a554a8a470a986373f97f3c72664396ab
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-06-24 10:47:36 +0100

    updated form

--- pages/components/Bridge/index.js
+++ pages/components/Bridge/index.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import styled from "styled-components";
+import { GlobalContext } from "../../../context/globalContext";
 import Button from "../Button/Connect";
 
 const Container = styled.div`
@@ -7,7 +8,7 @@ const Container = styled.div`
   background: #fff;
   min-height: 300px;
   width: 700px;
-  margin: 1.5rem 0;
+  margin: 4rem 0;
   padding: 1.5rem;
   @media (max-width: 768px) {
     width: 80%;
@@ -39,6 +40,9 @@ const FormGroup = styled.div`
 const LabelContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  label {
+    font-weight: 600;
+  }
 `;
 
 const Balance = styled.div``;
@@ -60,12 +64,110 @@ const AmountInput = styled.input`
   background-color: #f5f7fb;
 `;
 
+const AddressInput = styled.input`
+  width: 100%;
+  height: 100%;
+  border: 1px solid rgba(194, 201, 209, 0.5);
+  border-radius: 12px;
+  padding: 10px 25px;
+  background-color: #f5f7fb;
+  :disabled {
+    cursor: not-allowed;
+  }
+`;
+
 const ErrorMsg = styled.p`
   color: #e85151;
 `;
 
+const DropdownContainer = styled.div`
+  color: #000;
+  border-radius: 5px;
+  width: 100%;
+  cursor: pointer;
+`;
+
+const DropdownHeader = styled.div`
+  cursor: pointer;
+  border-radius: 12px;
+  border: 1px solid rgba(194, 201, 209, 0.5);
+  background-color: #f5f7fb;
+  height: 45px;
+  backdrop-filter: blur(1px);
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  &:div {
+    display: flex;
+  }
+  .chain_icon {
+    margin: 0px;
+    display: flex;
+    align-items: center;
+    width: 15px;
+    height: 15px;
+  }
+  :hover {
+    cursor: ${(props) => props.cursor || "pointer"};
+  }
+`;
+
+const DropdownListContainer = styled.div`
+  color: #000;
+  background: rgba(255, 255, 255, 0.8);
+  box-shadow: -10px 48.5967px 140px rgba(126, 123, 160, 0.2);
+  backdrop-filter: blur(19px);
+  border-radius: 10px;
+  overflow: hidden;
+  width: 15.5em;
+  position: absolute;
+  /* top: 10%; */
+  margin-top: 1.5rem;
+  z-index: 999;
+`;
+
+const DropdownList = styled("ul")`
+  list-style-type: none;
+`;
+
+const ListItem = styled("li")`
+  cursor: pointer;
+  font-weight: 400;
+  padding: 15px 0;
+  margin: 0 0 0 -40px;
+  display: flex;
+  align-items: center;
+  transition: all 0.3s linear;
+  backdrop-filter: blur(19px);
+
+  &:last-child {
+    margin-bottom: -16px;
+  }
+  &:hover {
+    background: #4500a0;
+    color: rgba(255, 255, 255, 1);
+  }
+`;
+
+const TokenIcon = styled("img")`
+  margin: 0 15px;
+  width: 25px;
+  height: 25px;
+  object-fit: contain;
+`;
+
+const Text = styled("p")`
+  font-size: 13px;
+  color: #91979d;
+  margin-left: 25px;
+  :disabled {
+    cursor: not-allowed;
+  }
+`;
+
 const Bridge = () => {
   const [isError, setIsError] = useState(false);
+  const { walletConnected } = useContext(GlobalContext);
 
   return (
     <>
@@ -77,21 +179,61 @@ const Bridge = () => {
           <Form>
             <FormGroup>
               <LabelContainer>
+                <label htmlFor="token">Token</label>
+              </LabelContainer>
+              <InputWrap>
+                <DropdownContainer>
+                  {walletConnected ? (
+                    <DropdownHeader>
+                      <Text>Connect Wallet First</Text>
+                    </DropdownHeader>
+                  ) : (
+                    <DropdownHeader cursor="not-allowed">
+                      <Text>Connect Wallet First</Text>
+                    </DropdownHeader>
+                  )}
+                </DropdownContainer>
+              </InputWrap>
+            </FormGroup>
+            <FormGroup>
+              <LabelContainer>
                 <label htmlFor="amount">Amount</label>
                 <Balance>Balance:--</Balance>
               </LabelContainer>
               <InputWrap>
-                <AmountInput type="number" id="amount" />
+                <AmountInput
+                  type="number"
+                  id="amount"
+                  placeholder="Enter an amount"
+                />
               </InputWrap>
               {isError ? <ErrorMsg>Error Message</ErrorMsg> : null}
+            </FormGroup>
+            <FormGroup>
+              <LabelContainer>
+                <label htmlFor="address">Receiving Address</label>
+              </LabelContainer>
+              <InputWrap>
+                {walletConnected ? (
+                  <AddressInput
+                    type="text"
+                    id="address"
+                    value="0xF2255c5F4dd0a2dfC4B65bab08EE27CA58333362"
+                  />
+                ) : (
+                  <AddressInput
+                    type="text"
+                    disabled
+                    id="address"
+                    placeholder="Connect Wallet First"
+                  />
+                )}
+              </InputWrap>
             </FormGroup>
           </Form>
         </FormContainer>
         <ButtonContainer>
-          <Button
-            height="60px"
-            padding="4px 16px"
-          />
+          <Button height="60px" padding="4px 16px" />
         </ButtonContainer>
       </Container>
     </>
