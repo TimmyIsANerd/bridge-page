223b35adcdb69c1091845adee581cc27a1f1bd2e
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-06-24 09:03:11 +0100

    added context to show/hide modal

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,35 @@
-import Head from 'next/head'
-import { useState } from 'react';
-import Bridge from './components/Bridge';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import styled from 'styled-components';
-import Header from './components/Header';
-import Modal from './components/Modal';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/globalContext';
+import Bridge from "./components/Bridge";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import styled from "styled-components";
+import Header from "./components/Header";
+import Modal from "./components/Modal";
 
 const BridgeContainer = styled.div`
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  width:100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 `;
 
-
 function Home() {
-  const [modal, setModal] = useState(false);
 
-  return(
+  const {showModal} = useContext(GlobalContext);
+
+  return (
     <>
-      <Head>
-        {/* Change Page Title Here */}
-        <title>Bridge Page</title>
-      </Head>
       <Navbar />
-      <Modal />
+      {showModal ? <Modal/> : null}
       <BridgeContainer>
         <Header />
         <Bridge />
       </BridgeContainer>
       <Footer />
     </>
-  )
+  );
 }
-
 
 export default Home;
