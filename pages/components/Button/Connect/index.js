32963728f53ac8fd3a92a1f71a37bc8319c62a5b
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-06-25 16:54:28 +0100

    updated connect button

--- pages/components/Button/Connect/index.js
+++ pages/components/Button/Connect/index.js
@@ -3,49 +3,55 @@ import { useContext } from "react";
 import { GlobalContext } from "../../../../context/globalContext";
 
 const ConnectButton = styled.button`
-    width: 100%;
-    height: ${(props) => props.height || "40px"};
-    background-color: ${(props) => props.backgroundColor || "#101010"};
-    border: none !important;
-    border-radius: 9999px;
-    outline: none !important;
-    text-align: center;
-    font-size: ${(props) => props.fontSize || "16px"};
-    color: ${(props) => props.color || "#fff"};
-    vertical-align: middle;
-    position: relative;
-    z-index: 1;
-    overflow: hidden;
-    padding:${(props) => props.padding || "0 18px"};
-    transition:0.5s;
-    :hover{
-        cursor:pointer;
-        background-color: ${(props) => props.hoverBackgroundColor || "#4500a0"};
-    }
-`
+  width: 100%;
+  height: ${(props) => props.height || "40px"};
+  background-color: ${(props) => props.backgroundColor || "#101010"};
+  border: none !important;
+  border-radius: 9999px;
+  outline: none !important;
+  text-align: center;
+  font-size: ${(props) => props.fontSize || "16px"};
+  color: ${(props) => props.color || "#fff"};
+  vertical-align: middle;
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+  padding: ${(props) => props.padding || "0 18px"};
+  transition: 0.5s;
+  :hover {
+    cursor: pointer;
+    background-color: ${(props) => props.hoverBackgroundColor || "#4500a0"};
+  }
+`;
 
-const Button = ({title,height,backgroundColor,color,hoverBackgroundColor,padding}) =>{
+const Button = ({
+  title,
+  height,
+  backgroundColor,
+  color,
+  hoverBackgroundColor,
+  padding,
+}) => {
+  const { showModalSwitch } = useContext(GlobalContext);
 
-    const {showModalSwitch} = useContext(GlobalContext);
+  function handleConnectClick() {
+    showModalSwitch(true);
+  }
 
-    function handleConnectClick(){
-        showModalSwitch(true);
-    }
-
-    return(
-        <>
-            <ConnectButton
-                height={height}
-                backgroundColor={backgroundColor}
-                color={color}
-                hoverBackgroundColor={hoverBackgroundColor}
-                padding={padding}
-                onClick={handleConnectClick}
-            >
-                {title ? title : 'Connect Wallet'}
-            </ConnectButton>
-        </>
-    )
-}
+  return (
+    <>
+      <ConnectButton
+        height={height}
+        backgroundColor={backgroundColor}
+        color={color}
+        hoverBackgroundColor={hoverBackgroundColor}
+        padding={padding}
+        onClick={handleConnectClick}
+      >
+        {title ? title : "Connect Wallet"}
+      </ConnectButton>
+    </>
+  );
+};
 
 export default Button;
